114a6eeb2e7a476718f78f8f2cfa7e79decfcdae
Vyshnavi-Kotha <123010313+Vyshnavi-Kotha@users.noreply.github.com>
2025-07-17 10:32:25 +0530

    Add files via upload

---
+++ UserController.java
@@ -0,0 +1,16 @@
+package com.example;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+}
